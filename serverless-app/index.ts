409a1978277dfbb528e4aed05e0142f5f262c034
Gerdisch <github@jesslex.com>
2020-08-05 16:42:32 -0500

    component resource for s3 and functions

--- serverless-app/index.ts
+++ serverless-app/index.ts
@@ -2,14 +2,10 @@
  * Using Pulumi, create and deploy a serverless application that processes uploads to a storage
  * bucket and builds an index of the files in a database table.
  * 
- * Plan:
- * - (DONE) Get s3 bucket configuration working.
- *  - (DONE) Have it push some files too. There's Pulumi example of doing that.
- * - (DONE) Get DynamoDB or similar working.
- * - Get Lambda working that reads S3 files and pushes data to DB
- *  - Detects files that are there
- *  - Detects new files as they are added
- * 
+ * TODOs
+ * -  Figure out how to get the file generation stuff to run after the magic functions are created and ready.
+ * - Maybe change things from using two different triggers (create and delete) and functions to one trigger (any event) and
+ *   one function that handles the different events (e.g. not just create and delete but update)
  */
 
 import * as pulumi from "@pulumi/pulumi";
@@ -17,12 +13,9 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 import * as util from 'util';
 import * as AWS  from 'aws-sdk';
+import { S3LambdaDynamo } from './s3lambda'
 
-/**** S3 Bucket *****/
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("serverless-app-bkt", {
-    forceDestroy: true, // destroys bucket even if there are files in there
-});
+
 
 /**** DynamoDB *****/
 const dbTableName = "s3object-table" 
@@ -51,86 +44,24 @@ const fileTable = new aws.dynamodb.Table(dbTableName, {
     name: dbTableName, // assures the table name is known. to-do: figure out how to use generated table name in the magic function.
 });
 
-/***** Lambda via magic function love *****/
-// Helper function that the magic function calls to push the item.
-async function pushItem(dbName: string, cleanKey: string, eventTime: string) {
-    const dbClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'})
-    // DynamoDB entry
-    let dbParams = {
-        Item: {
-            ObjectKey: cleanKey,
-            TimeStamp: eventTime,
-        },
-        TableName: dbTableName,
-    }
-    console.log("Push dbParams",dbParams)
+const s3lambdaComplex = new S3LambdaDynamo("s3lambdaComplex", dbTableName, {})
 
-    // Push the DB entry
-    await dbClient.put(dbParams, function(err, data) {
-        if (err) {
-            console.log("DB PUT ERROR",err);
-        } else {
-            console.log("DB PUT SUCCESS", "TABLE: "+dbTableName, "KEY: "+cleanKey, "TIME: "+eventTime);
-        };
-    });
-}
-
-// Deletes item from DB - called when a file is removed from S3
-async function deleteItem(dbName: string, cleanKey: string) {
-    const dbClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'})
-    // DynamoDB entry
-    let dbParams = {
-        Key: {
-            ObjectKey: cleanKey
-        },
-        TableName: dbTableName,
-    }
-    console.log("Delete dbParams",dbParams)
-
-    // delete the DB entry
-    await dbClient.delete(dbParams, function(err, data) {
-        if (err) {
-            console.log("DB DELETE ERROR",err);
-        } else {
-            console.log("DB DELETE SUCCESS", "TABLE: "+dbTableName, "KEY: "+cleanKey);
-        };
-    });
-}
-
-
-// When bucket objects are created (e.g. a file is uploaded) this will invoke the defined (lambda) function to add the item to the DB.
-bucket.onObjectCreated("addObjectLambda", event => {
-    // Read options from the event parameter.
-    console.log("S3 Create event:\n", util.inspect(event, {depth: 5}));
-    for (const record of event.Records || [])  {
-        const cleanKey = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
-        const eventTime = record.eventTime;
-        pushItem(dbTableName, cleanKey, eventTime);
-    };
-});
-
-// When bucket objects are deleted this will invoke the defined (lambda) function to remove the item from the DB.
-bucket.onObjectRemoved("deleteObjectLambda", event => {
-    // Read options from the event parameter.
-    console.log("S3 Delete event:\n", util.inspect(event, {depth: 5}));
-    for (const record of event.Records || [])  {
-        const cleanKey = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
-        deleteItem(dbTableName, cleanKey);
-    };
-});
-
+/* This doesn't work.
+ * It seems to continue the moment the first element of S3LambdaDynamo is ready and doesn't wait until all the elements -
+ * specifically the lambda functions are done.
+ * So the file creations are not processed .... hmmmmm
+ * 
 // push some sample files to the S3 bucket after everything is set up.
-/*** doesn't work currently due to parallell processing. maybe component resource stuff will fix it automagically.
 let filesDir = "samplefiles"; // directory for some files to load as part of the S3 set up.
 for (let item of require("fs").readdirSync(filesDir)) {
     let filePath = require("path").join(filesDir, item);
     let object = new aws.s3.BucketObject(item, {
-      bucket: bucket,
+      bucket: s3lambdaComplex.bucketName,
       source: new pulumi.asset.FileAsset(filePath),     // use FileAsset to point to a file
-    });
+    }, {dependsOn: [s3lambdaComplex, fileTable]});
 }
-***/
+*/
 
 // Export some data
-export const S3bucket = bucket.id;
+export const S3bucket = s3lambdaComplex.bucketName;
 export const DynamoDbTable = fileTable.id;
---
+++ serverless-app/s3lambda.ts
@@ -0,0 +1,110 @@
+/*
+ * Builds the following:
+ * - S3 bucket to which files are uploaded.
+ * - Lambda function triggered by S3 file uploads that writes file name and timestamp to provided DynamoDB table.
+ * 
+ * TODO:
+ * - Currently have to keep this code and the DyanmoDB code in synch since they both need to know table schema. 
+ *   Would be neat to be able to pass that information into here. I'm thinking there's a way to evaluate parameters and use them as the schema keys.
+ * 
+ */
+
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+import * as AWS  from 'aws-sdk'
+import { output } from "@pulumi/aws/types";
+
+export class S3LambdaDynamo extends pulumi.ComponentResource {
+
+    public bucketName: pulumi.Output<string>;
+
+    //constructor(dbTableName: string, opts: pulumi.ComponentResourceOptions = {}) {
+    constructor(name: string, dbTableName: string, opts: pulumi.ComponentResourceOptions = {}) {
+
+        // Register this component 
+        super("exercise:S3LambdaDynamo", name, opts);
+
+        /**** S3 Bucket *****/
+        // Create an AWS resource (S3 Bucket)
+        let bucket = new aws.s3.Bucket("serverless-app-bkt", {
+            forceDestroy: true, // destroys bucket even if there are files in there
+        }, {parent: this});
+
+        // Create a property for the bucket name that was created
+        this.bucketName = bucket.id;
+
+        // For dependency tracking, register output properties for this component
+        this.registerOutputs({
+            bucketName: this.bucketName,
+        });
+
+        /***** Lambda via magic function love *****/
+        // Helper function that the magic function calls to push the item.
+        async function pushItem(dbName: string, cleanKey: string, eventTime: string) {
+            const dbClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'})
+            // DynamoDB entry
+            let dbParams = {
+                Item: {
+                    ObjectKey: cleanKey,
+                    TimeStamp: eventTime,
+                },
+                TableName: dbTableName,
+            }
+            console.log("Push dbParams",dbParams)
+
+            // Push the DB entry
+            await dbClient.put(dbParams, function(err, data) {
+                if (err) {
+                    console.log("DB PUT ERROR",err);
+                } else {
+                    console.log("DB PUT SUCCESS", "TABLE: "+dbTableName, "KEY: "+cleanKey, "TIME: "+eventTime);
+                };
+            });
+        }
+
+        // Deletes item from DB - called when a file is removed from S3
+        async function deleteItem(dbName: string, cleanKey: string) {
+            const dbClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'})
+            // DynamoDB entry
+            let dbParams = {
+                Key: {
+                    ObjectKey: cleanKey
+                },
+                TableName: dbTableName,
+            }
+            console.log("Delete dbParams",dbParams)
+
+            // delete the DB entry
+            await dbClient.delete(dbParams, function(err, data) {
+                if (err) {
+                    console.log("DB DELETE ERROR",err);
+                } else {
+                    console.log("DB DELETE SUCCESS", "TABLE: "+dbTableName, "KEY: "+cleanKey);
+                };
+            });
+        }
+
+
+        // When bucket objects are created (e.g. a file is uploaded) this will invoke the defined (lambda) function to add the item to the DB.
+        bucket.onObjectCreated("addObjectLambda", event => {
+            // Read options from the event parameter.
+            console.log("S3 Create event:\n", event);
+            for (const record of event.Records || [])  {
+                const cleanKey = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
+                const eventTime = record.eventTime;
+                pushItem(dbTableName, cleanKey, eventTime);
+            };
+        });
+
+        // When bucket objects are deleted this will invoke the defined (lambda) function to remove the item from the DB.
+        bucket.onObjectRemoved("deleteObjectLambda", event => {
+            // Read options from the event parameter.
+            console.log("S3 Delete event:\n", event);
+            for (const record of event.Records || [])  {
+                const cleanKey = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
+                deleteItem(dbTableName, cleanKey);
+            };
+        });
+    };
+};
