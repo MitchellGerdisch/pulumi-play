6373975c58f68bcfd81d3f2e029b46ac63cc2b4c
MitchellGerdisch <mitchell.gerdisch@jesslex.com>
2020-10-06 15:25:19 -0500

    dynamodb bits

---
+++ aws-api/README.md
@@ -0,0 +1,5 @@
+# Creating an API with AWS: Lambda, DynamoDB and API Gateway
+
+This folder contains [Pulumi](https://www.pulumi.com/) infrastructure as code that stands up the environment described in [Lambda, DynamoDB, and API Gateway for your Jamstack App](https://www.netlify.com/guides/creating-an-api-with-aws-lambda-dynamodb-and-api-gateway). The goal is to augment this excellent tutorial with some infrastructure automation.
+
+##
--- aws-api/index.ts
+++ aws-api/index.ts
@@ -1,9 +1,46 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+/*
+ * This example code mirrors the example documented in this guide here: https://www.netlify.com/guides/creating-an-api-with-aws-lambda-dynamodb-and-api-gateway
+ * Hence you'll see comments that reflect the sections of that guide.
+ */
+import * as pulumi from '@pulumi/pulumi';
+import * as aws from '@pulumi/aws';
+import * as awsx from '@pulumi/awsx';
+import * as random from '@pulumi/random';
+import { RandomUuid } from '@pulumi/random';
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+// Let's build a DynamoDB
+const tableName = 'Restaurants';
+const table = new aws.dynamodb.Table(tableName, {
+  attributes: [
+    {
+      name: 'Id',
+      type: 'S',
+    },
+  ],
+  billingMode: 'PROVISIONED',
+  hashKey: 'Id',
+  readCapacity: 10,
+  writeCapacity: 5,
+  name: tableName,
+});
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+// Let's prepopulate with some data.
+// This is not necessary in general since external systems will likely be the ones populating data, but the guide included some prepopulation of entries.
+// Plus it shows some fun bits of Pulumi IaC - namely, being able to use natural language for loops and other concepts like conditional assignment, etc.
+// This leverages the Random provider which includes a RandomPet to create somewhat normal sounding names instead of random strings of characters.
+const numPreItems = 3;
+for (let i = 0; i < numPreItems; i++) {
+  const randUuid = new random.RandomUuid('uuid' + i);
+  const randName = new random.RandomPet('name' + i);
+  const freeDel = i % 2 == 0 ? true : false;
+  const item = pulumi.interpolate`{
+        "Id": {"S": "${randUuid.result}"},
+        "Name": {"S": "${randName.id}"},
+        "FreeDelivery": {"BOOL": ${freeDel}}
+    }`;
+  const tableItem = new aws.dynamodb.TableItem('TableItem' + i, {
+    tableName: table.name,
+    hashKey: table.hashKey,
+    item: item,
+  });
+}
