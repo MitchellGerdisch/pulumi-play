778056394159b49462586b66848bb6e30e99a267
MitchellGerdisch <mitchell.gerdisch@jesslex.com>
2020-10-07 09:13:29 -0500

    done

--- aws-api/README.md
+++ aws-api/README.md
@@ -1,5 +1,13 @@
 # Creating an API with AWS: Lambda, DynamoDB and API Gateway
 
-This folder contains [Pulumi](https://www.pulumi.com/) infrastructure as code that stands up the environment described in [Lambda, DynamoDB, and API Gateway for your Jamstack App](https://www.netlify.com/guides/creating-an-api-with-aws-lambda-dynamodb-and-api-gateway). The goal is to augment this excellent tutorial with some infrastructure automation.
+This folder contains [Pulumi](https://www.pulumi.com/) infrastructure as code that stands up the environment described in [Lambda, DynamoDB, and API Gateway for your Jamstack App](https://www.netlify.com/guides/creating-an-api-with-aws-lambda-dynamodb-and-api-gateway).
 
-##
+Although this example ends with making the API URL and Key available, it wouldn't be very difficult to extend the example to push the information to Netlify to complete the pipeline.
+
+## Basic Architecture
+
+- Deploy DynamoDB
+  - Prepopulate it with some data
+- Deploy API gateway
+  - Use a Lambda event handler for the API
+  - Enable an API key
--- aws-api/index.ts
+++ aws-api/index.ts
@@ -1,6 +1,6 @@
 /*
- * This example code mirrors the example documented in this guide here: https://www.netlify.com/guides/creating-an-api-with-aws-lambda-dynamodb-and-api-gateway
- * Hence you'll see comments that reflect the sections of that guide.
+ * This example code is inspired by the configuration documented in this guide: https://www.netlify.com/guides/creating-an-api-with-aws-lambda-dynamodb-and-api-gateway
+ * Instead of manually building the API gateway and related parts step-by-step, this file leverages Pulumi infrastructure as code to deploy the system.
  */
 import * as pulumi from '@pulumi/pulumi';
 import * as aws from '@pulumi/aws';
@@ -9,7 +9,6 @@ import * as random from '@pulumi/random';
 import * as AWS from 'aws-sdk';
 
 ///// DynamoDB Set Up //////
-
 // Create a DynamoDB table
 const tableName = 'Restaurants';
 const table = new aws.dynamodb.Table(tableName, {
@@ -26,19 +25,27 @@ const table = new aws.dynamodb.Table(tableName, {
   name: tableName,
 });
 
-// Prepopulate with some data.
-// This is generally not necessary since external systems will likely be the ones populating data, but for the sake of showing examples in the guide, let's add some random-ish data.
-// Plus it shows some fun bits of Pulumi IaC - namely, being able to use natural language for loops and other concepts like conditional assignment, etc.
-// This leverages the Random provider to create the UUID and Name.
+// Prepopulate with some data as per the guide.
+// Although this is, arguably, throw-away code - the API could/would support a POST action to allow for populating the restaurants DB,
+// it allows for quick testing and as a side effect, it further demonstrates the power of Pulumi IaC.
+// For example, since you are using an actual programming language, you can write logic using for loops and other concepts like conditional assignment, etc.
 const numPreItems = 3;
 for (let i = 0; i < numPreItems; i++) {
-  const randUuid = new random.RandomUuid('uuid' + i);
-  const randName = new random.RandomPet('name' + i);
-  const freeDel = i % 2 == 0 ? true : false;
+  const randUuid = new random.RandomUuid('uuid' + i).result; // get a random UUID
+  const randName = new random.RandomPet('name' + i).id; // get a random name
+  const rating = new random.RandomInteger('rating' + i, { min: 1, max: 5 }).result; // get a random rating
+  const freeDel = i % 2 == 0 ? true : false; // just flip flop free delivery setting
+  const delTime = new random.RandomShuffle('delTime' + i, {
+    inputs: ['10', '20', '30', '45', '60'],
+    resultCount: 1,
+  }).results; // get a random delivery time
+  // other attributes like menu could also be programmatically added
   const item = pulumi.interpolate`{
-        "id": {"S": "${randUuid.result}"},
-        "name": {"S": "${randName.id}"},
-        "freeDelivery": {"BOOL": ${freeDel}}
+        "id": {"S": "${randUuid}"},
+        "name": {"S": "${randName}"},
+        "freeDel": {"BOOL": ${freeDel}},
+        "rating": {"N": "${rating}"},
+        "delTime": {"N": "${delTime}"}
     }`;
   const tableItem = new aws.dynamodb.TableItem('TableItem' + i, {
     tableName: table.name,
@@ -49,9 +56,11 @@ for (let i = 0; i < numPreItems; i++) {
 
 ///// API Gateway and Lambda Event Handler //////
 // This next part leverages Pulumi's Crosswalk for AWS (https://www.pulumi.com/docs/guides/crosswalk/aws/).
-// In a nutshell, Crosswalk simplifies complex multistep and resource configuration use-cases.
+// In a nutshell, Crosswalk simplifies complex multistep resource configuration use-cases.
 
-// Helper function to retrieve specific customer data from DB
+// This function is the event handler code that Pulumi will be automatically pushed into Lambda as part of the API gateway declaration below.
+// Of course, this function and additional handler functions for other API methods (e.g. POST method to add restaurants to the DB, or DELETE method to remove restaurants)
+// could be stored and maintaned in a separate module file. But for the sake of readability it is kept in the main file.
 async function getRestaurants(dbName: string) {
   const dbClient = new AWS.DynamoDB.DocumentClient();
   // DynamoDB entry
@@ -68,31 +77,45 @@ async function getRestaurants(dbName: string) {
       }
     })
     .promise();
-  return dbContents.Items;
+  return {
+    statusCode: 200,
+    body: JSON.stringify(dbContents.Items),
+  };
 }
 
+// Instantiate an API gateway and the related Lambda event handler.
+// In keeping with the above referenced guide, the API is simple get-me-all-the-restaurants API.
+// Of course additional routes and event handlers could be added to handle different methods and parameter options.
 const api = new awsx.apigateway.API('restaurants-api', {
   routes: [
     {
+      // GET method to retrieve all the restaurants
       path: '/restaurants',
       method: 'GET',
+      apiKeyRequired: true,
       eventHandler: async (event) => {
-        const result = await getRestaurants(tableName);
-        return {
-          statusCode: 200,
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Methods': '*',
-            'Access-Control-Allow-Credentials': '*',
-          },
-          body: JSON.stringify(result),
-        };
+        return await getRestaurants(tableName);
       },
     },
   ],
 });
 
-// Export the API call for quick testing
-// This can also be passed into the application, etc as per the guide
+// Add a usage plan and api key as per the guide.
+const apikeys = awsx.apigateway.createAssociatedAPIKeys('restaurant-api-keys', {
+  apis: [api],
+  apiKeys: [
+    {
+      name: 'restaurant-api-key',
+    },
+  ],
+});
+
+// Export the API call and API key for testing
+// Although the API key is sensitive data, for this use-case it is exported to the command line for testing purposes.
+// That said, Pulumi allows you avoid displaying sensitive data via it's secrets support.
 export const apiUrl = pulumi.interpolate`${api.stage.invokeUrl}/restaurants`;
+export const apiKeyValue = apikeys.keys[0].apikey.value;
+
+// At this point, these outputs as well as others are available to other systems to complete a pipeline.
+// The guide uses the URL and key to configure Nuxt and Netlify. These values are programmatically accessible.
+// Similarly, a Pulumi provider could easily be created that interacts with Netlify as well.
